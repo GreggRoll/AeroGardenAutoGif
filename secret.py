5b4fe2a7e8a6c201d0a7e291b3774a7c1109c72d
GreggRoll <adamsg_53@yahoo.com>
2020-09-08 12:59:57 -0400

    rm secret

--- secret.py
+++
@@ -1,5 +0,0 @@
-# personal details 
-consumer_key ="a6PgX9HTWTvfZbOf2iuYuBhWW"
-consumer_secret ="wgHUyZWQjeOimx2yaBMyaawnMTOesNaMgWRYtmx8IGPWs8NJwg"
-access_token ="1047902175870046209-GlNwLGDvXvduIFSEXDUG8KK8HWIWMD"
-access_token_secret ="eV9yjFxl8ufODqT1jagrsAyDyqpwv8BwAAbmNVF9Dd0Vc"
